27c6333af3cda845fd261190a431e2905e94ae6c
vincentgo6 <go.vincent6@gmail.com>
2020-09-15 20:05:12 -0400

    Updates

---
+++ precache-manifest.e17a5c6627cc26fce27143d75e5d7788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1319495f8d91aecb3da2acfc1852f5c8",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "31bd980b1450c7c5a1a2",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "a7a2bcae1c436a37399b",
+    "url": "/box-office/static/js/2.8d79446e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.8d79446e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bd980b1450c7c5a1a2",
+    "url": "/box-office/static/js/main.0ac90942.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
